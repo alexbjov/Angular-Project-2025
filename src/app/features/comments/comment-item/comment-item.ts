d483f2c6cbccbde5dc739c017722196a2c263b78
alexbj <ajoveski@gmail.com>
2025-08-17 23:57:16 +0200

    Add comments board and item

--- src/app/features/comments/comment-item/comment-item.ts
+++ src/app/features/comments/comment-item/comment-item.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Comment } from "../../../models/comment.model";
 
 @Component({
   selector: 'app-comment-item',
   imports: [],
   templateUrl: './comment-item.html',
-  styleUrl: './comment-item.css'
+  styleUrl: './comment-item.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommentItem {
-
+  comment!: Comment;
 }
--- src/app/features/comments/comments-board/comments-board.ts
+++ src/app/features/comments/comments-board/comments-board.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from "@angular/common";
 import { Component } from '@angular/core';
+import { Observable } from "rxjs";
+import { Comment } from "../../../models/comment.model";
+import { CommentsService } from "../../../services/comments.service";
+import { CommentItem } from "../comment-item/comment-item";
 
 @Component({
   selector: 'app-comments-board',
-  imports: [],
+  imports: [CommonModule, CommentItem],
   templateUrl: './comments-board.html',
   styleUrl: './comments-board.css'
 })
 export class CommentsBoard {
+  comments: Comment[] = [];
+  comments$!: Observable<Comment[]>;
 
+  constructor(private commentsService: CommentsService) {
+    this.comments$ = this.commentsService.getComments();
+  }
 }
