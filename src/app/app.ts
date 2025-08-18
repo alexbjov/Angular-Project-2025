515e0ff3f0b26aea1a3dfe5f4675136578458871
alexbj <ajoveski@gmail.com>
2025-08-18 13:39:59 +0200

    Provide services to the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from "./services/auth.service";
+import { BooksService } from "./services/books.service";
+import { CommentsService } from "./services/comments.service";
 import { Footer } from "./shared/footer/footer";
 import { Header } from "./shared/header/header";
 
@@ -7,7 +10,8 @@ import { Header } from "./shared/header/header";
   selector: 'app-root',
   imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [AuthService, BooksService, CommentsService]
 })
 export class App {
   protected readonly title = signal('fan-books-club');
