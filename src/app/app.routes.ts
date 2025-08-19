e51ea93be4420c2063b687aa2cdc8c90edd3a05a
alexbj <ajoveski@gmail.com>
2025-08-19 19:57:24 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Login } from "./features/auth/login/login";
+import { Register } from "./features/auth/register/register";
+import { Home } from "./features/home/home";
+import { NotFound } from "./shared/not-found/not-found";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
+  },
+  {
+    path: 'logout',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFound
+  }
+];
