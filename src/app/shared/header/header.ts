b54515d3d85b29c1dd27e065d944e44371b4b19b
alexbj <ajoveski@gmail.com>
2025-08-19 21:48:38 +0200

    Add logout button to the header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class Header {
-
+  authService = inject(AuthService);
+  currentUser = this.authService.currentUser();
+  isLogged = this.authService.isLoggedIn();
 }
