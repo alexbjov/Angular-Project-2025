d9568cee937fdb75c3a4a15851a79983dde24814
alexbj <ajoveski@gmail.com>
2025-08-20 21:14:47 +0200

    Add reactive login form

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router, RouterLink } from "@angular/router";
 import { AuthService } from "../../../services/auth.service";
 
@@ -10,48 +10,25 @@ import { AuthService } from "../../../services/auth.service";
   styleUrl: './login.css'
 })
 export class Login {
-  router = inject(Router);
-  authService = inject(AuthService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
-  loginForm: FormGroup<{
-    email: FormControl<string>;
-    password: FormControl<string>;
-  }>;
-
+  loginForm: FormGroup;
 
   constructor() {
-    this.loginForm = new FormGroup({
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.email],
-        nonNullable: true,
-      }),
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(4)],
-        nonNullable: true,
-      }),
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z][a-zA-Z0-9._-]{2,}[a-zA-Z]@[a-z]{2,}\.[a-z]{2,}$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-    console.log("Router:", this.router);
-    console.log("Auth:", this.authService);
   }
 
-  reset() {
-    this.loginForm.reset()
+  get email(): AbstractControl<any, any> | null {
+    return this.loginForm.get('email');
   }
 
-  get email(): FormControl<string> {
-    return this.loginForm.get('email') as FormControl<string>;
-  }
-
-  get password(): FormControl<string> {
-    return this.loginForm.get('password') as FormControl<string>;
-  }
-
-  get value(): { email: string; password: string } {
-    return this.loginForm.value as { email: string; password: string };
-  }
-
-  get invalid(): boolean {
-    return this.loginForm.invalid;
+  get password(): AbstractControl<any, any> | null {
+    return this.loginForm.get('password');
   }
 
   get isEmailValid(): boolean {
@@ -67,8 +44,8 @@ export class Login {
       return 'Email is required!';
     }
 
-    if (this.email?.errors?.['email']) {
-      return 'Email not valid!';
+    if (this.email?.errors?.['pattern']) {
+      return 'Email is not valid!';
     }
 
     return '';
@@ -80,7 +57,7 @@ export class Login {
     }
 
     if (this.password?.errors?.['minlength']) {
-      return 'Password must be at least 4 characters!';
+      return 'Password must be at least 6 characters!';
     }
 
     return '';
@@ -88,11 +65,9 @@ export class Login {
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      console.log("Valid form!");
-      console.log("loginForm.value:", this.loginForm.value);
       const { email, password } = this.loginForm.value;
 
-      this.authService.login(email!, password!).subscribe({
+      this.authService.login(email, password).subscribe({
         next: () => {
           this.router.navigate(['/home']);
         },
