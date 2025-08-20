20e2ebf2408d54425e4f02b88f54d22c4f8493c4
alexbj <ajoveski@gmail.com>
2025-08-20 21:25:58 +0200

    Improve app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from "./services/auth.service";
 import { BooksService } from "./services/books.service";
-import { CommentsService } from "./services/comments.service";
 import { Footer } from "./shared/footer/footer";
 import { Header } from "./shared/header/header";
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  providers: [AuthService, BooksService, CommentsService]
+  providers: [AuthService, BooksService]
 })
 export class App {
   protected readonly title = signal('fan-books-club');
