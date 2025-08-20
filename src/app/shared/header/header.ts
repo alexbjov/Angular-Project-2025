3ca02c68f6f8cf8a969141812eaaa9f546ff6b08
alexbj <ajoveski@gmail.com>
2025-08-20 21:20:47 +0200

    Add new profile button

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,15 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
-  changeDetection: ChangeDetectionStrategy.Default
 })
 export class Header {
   authService = inject(AuthService);
-  currentUser = this.authService.currentUser();
-  isLogged = this.authService.isLoggedIn();
+  router = inject(Router);
+
+  readonly isLogged = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        localStorage.removeItem('currentUser');
+        console.log('Logout failed', err);
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
