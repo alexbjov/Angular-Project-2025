367fd1f6769de01679d7b9a8535185f4a6163180
alexbj <ajoveski@gmail.com>
2025-08-20 21:18:08 +0200

    Add new routes for profile and new book

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { About } from "./features/about/about";
 import { Login } from "./features/auth/login/login";
 import { Register } from "./features/auth/register/register";
 import { Contact } from "./features/contact/contact";
-import { Home } from "./features/home/home";
+import { authGuard } from "./guards/auth.guard";
 import { NotFound } from "./shared/not-found/not-found";
 
 export const routes: Routes = [
@@ -14,15 +14,7 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    component: Home
-  },
-  {
-    path: 'login',
-    component: Login
-  },
-  {
-    path: 'register',
-    component: Register
+    loadComponent: () => import('./features/home/home').then(c => c.Home)
   },
   {
     path: 'about',
@@ -33,9 +25,31 @@ export const routes: Routes = [
     component: Contact
   },
   {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
+  },
+  {
     path: 'logout',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile').then(c => c.Profile),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'books',
+    loadComponent: () => import('./features/mybooks/books-board/books-board').then(c => c.BooksBoard)
+  },
+  {
+    path: 'add-book',
+    loadComponent: () => import('./features/mybooks/add-book/add-book').then(c => c.AddBook),
+    canActivate: [authGuard]
   },
   {
     path: '**',
